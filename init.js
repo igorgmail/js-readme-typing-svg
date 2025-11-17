be19b04c1ab55be9bcd7419879109926a2e763c1
Igor.K <i.korneev@prime-wood.ru>
2025-11-17 17:52:40 +0700

    init project

---
+++ init.js
@@ -0,0 +1,22 @@
+// --------------- DEMO --------------------
+document.addEventListener("DOMContentLoaded", () => {
+  createMultilineBlockTypingSVG({
+    lines: [
+      "🅷🅴🆈 My name`s Igor",
+      "Today is Wednesday, $DATE(YYYY-MM-DD)",
+			"it`s a great day to code."
+    ],
+    printSpeed: 50,
+    fontSize: 20,
+    background: "#81BECE",
+    color: "1A5975",
+    width: 1000,
+    height: 100,
+    verticalAlign: "middle",
+    horizontalAlign: "center",
+    typingMode: "static", // expand | static
+    eraseMode: "line", // line | block | wipe-down | fade
+		multiLine: false,
+  });
+});
+
